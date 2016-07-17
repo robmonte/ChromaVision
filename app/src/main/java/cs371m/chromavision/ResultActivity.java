dce3ae847dc78228d192c198025a06f948efc829
Yajieniu <fineyear3714@gmail.com>
2016-07-17 18:28:05 -0500

    A clean version

--- app/src/main/java/cs371m/chromavision/ResultActivity.java
+++ app/src/main/java/cs371m/chromavision/ResultActivity.java
@@ -14,71 +14,23 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ResultActivity extends AppCompatActivity {
 
     ImageView mImageView;
     Bitmap resultBitmap;
-    public MainMenuActivity mMain;
+    MainMenuActivity mMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-
-//        Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
-
-        Intent intent = getIntent();
-//        resultBitmap = intent.getParcelableExtra("BitmapImage");
         mImageView = (ImageView) findViewById(R.id.resultImage);
         System.out.println("in result activity.");
-//        if (resultBitmap != null) {
-        getBitmapfromUri(mMain.photoUri);
-//            mImageView.setImageBitmap(resultBitmap);
-
+//        File file = new File(mMain.photoUri.getPath());
     }
 
-    public void getBitmapfromUri(Uri pickedImage)
-    {
-        // Let's read picked image path using content resolver
-        String[] filePath = { MediaStore.Images.Media.DATA };
-        File picture = new File(""+mMain.photoUri);
-        System.out.println("test");
-        Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
-        cursor.moveToFirst();
-        String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
-        mImageView.setImageBitmap(bitmap);
-
-        // Do something with the bitmap
-
-
-        // At the end remember to close the cursor or you will end with the RuntimeException!
-        cursor.close();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        System.out.println("in result activity, for gallery picture.");
-        if (requestCode == Activity.RESULT_OK && resultCode == mMain.GALLERY_REQUEST_CODE)
-        {
-            Uri pickedImage = data.getData();
-            // Let's read picked image path using content resolver
-            String[] filePath = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
-            cursor.moveToFirst();
-            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
-
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
-            mImageView.setImageBitmap(bitmap);
-
-            cursor.close();
-        }
-    }
 }
